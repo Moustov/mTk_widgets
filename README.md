186c1e16dee03ebf04255d173f72352be5a69e26
chris <christophe.moustier@free.fr>
2023-03-16 09:01:22 +0100

    editable tree cells

--- README.md
+++ README.md
@@ -15,8 +15,16 @@ Overriden tkinter widgets
 Editable TreeView
 
 # Release Notes
+* 16/MAR/23
+  * editable tree cells
+  * package 0.3.x
+* 15/MAR/23
+  * horizontal scroller on double-click
+  * right-click menu
+  * package 0.2.0
 * 11/MAR/23
   * Editable TreeView
   * package 0.0.x
 
 # Additional links
+* Royalty free icons: https://www.vectorstock.com/royalty-free-vector/25-multimedia-blue-icon-pack-vector-45138316
--- moustovtkwidgets_lib/mtk_edit_table.py
+++ moustovtkwidgets_lib/mtk_edit_table.py
@@ -1,7 +1,7 @@
 from functools import partial
-from tkinter import Tk, Button, Scrollbar
+from tkinter import Tk, Button, Scrollbar, Menu
 from tkinter.constants import *
-from tkinter.ttk import Treeview, Entry
+from tkinter.ttk import Treeview, Entry, Frame
 
 
 # see https://www.youtube.com/watch?v=n5gItcGgIkk
@@ -22,9 +22,15 @@ class mtkEditTable(Treeview):
     def __init__(self, master, **kwargs):
         self.frame = master
         self.debug = False
+        self.edit_frame = None
+        self.horscrlbar = None
+        self.edit_entry = None
+
         self.columns = kwargs["columns"]
         self.column_titles = None
         self.cells = None
+        self.rowID = None
+        self.current_cell_value = None
         # handling extra params
         if "column_titles" in kwargs.keys():
             self.column_titles = kwargs["column_titles"]
@@ -37,6 +43,7 @@ class mtkEditTable(Treeview):
         # events
         print("<Double-1>")
         self.bind("<Double-1>", self._on_double_click)
+        self.bind("<Button-3>", self._on_right_click)
         # set layout
         if self.column_titles:
             self.column("#0", width=0, stretch=NO)
@@ -48,6 +55,43 @@ class mtkEditTable(Treeview):
             self.set_data(self.cells)
         else:
             self.clear_data()
+        # https://tkdocs.com/tutorial/menus.html
+        self.menu = Menu(self.frame, tearoff=0)
+        self.menu.add_command(label="Copy under", command=self.clone_after_current_row)
+        self.menu.add_command(label="New empty line under", command=self.new_after_current_row)
+        self.menu.add_separator()
+        self.menu.add_command(label="Remove current", command=self.remove_current)
+        # text & image : menu.add_command(label=txt, image=self._img4, compound='left', command=cmd)
+
+    def _on_right_click(self, event):
+        self.rowID = self.identify('item', event.x, event.y)
+        if self.rowID:
+            self.selection_set(self.rowID)
+            self.focus_set()
+            self.focus(self.rowID)
+            # self.menu.tk_popup(event.x_root, event.y_root)
+            self.menu.post(event.x_root, event.y_root)
+            self.current_cell_value = self.get_cell_value(event)
+
+    def remove_current(self):
+        """
+        must be prepared with _on_right_click()
+        """
+        self.delete(self.rowID)
+
+    def clone_after_current_row(self):
+        """
+        must be prepared with _on_right_click()
+        """
+        self.insert(parent="", index=int(self.rowID) + 1, iid=str(int(self.rowID) + 10000), text="",
+                    values=("", self.current_cell_value))
+
+    def new_after_current_row(self):
+        """
+        must be prepared with _on_right_click()
+        """
+        self.insert(parent="", index=int(self.rowID) + 1, iid=str(int(self.rowID) + 10000), text="",
+                    values=("", self.current_cell_value))
 
     def clear_data(self):
         self.cells = None
@@ -73,6 +117,24 @@ class mtkEditTable(Treeview):
             res[i] = data
         return res
 
+    def get_cell_value(self, event):
+        col_index = self.identify_column(event.x)
+        selected_row_iid = self.focus()
+        selected_values = self.item(selected_row_iid)
+        values = selected_values.get("values")
+        col_number = int(col_index[1:]) - 1
+        return values[col_number]
+
+    def get_cell_dimensions(self, event) -> ():
+        col_index = self.identify_column(event.x)
+        print("col_index", col_index)
+        selected_row_iid = self.focus()
+        if int(col_index[1:]) > 0:
+            col_number = int(col_index[1:]) - 1
+            return self.bbox(selected_row_iid, col_number)
+        else:
+            return self.bbox(selected_row_iid, col_index)
+
     def _on_double_click(self, event):
         """
         displays an entry field on top of the double-clicked cell
@@ -83,30 +145,46 @@ class mtkEditTable(Treeview):
         region_clicked = self.identify_region(event.x, event.y)
         if self.debug:
             print("region double clicked", region_clicked, event)
+        col_index = self.identify_column(event.x)
+        selected_row_iid = self.focus()
+        selected_values = self.item(selected_row_iid)
+        cell_box = self.get_cell_dimensions(event)
+        print("cell_box", cell_box)
+        col_number = 0
         if region_clicked == "cell":
-            col_index = self.identify_column(event.x)
-            selected_row_iid = self.focus()
-            selected_values = self.item(selected_row_iid)
             values = selected_values.get("values")
             col_number = int(col_index[1:]) - 1
-            cell_value = values[col_number]
-            cell_box = self.bbox(selected_row_iid, col_number)
+            cell_value = self.get_cell_value(event)
             if self.debug:
-                print("cell_box", cell_box)
-            edit_entry = Entry(self.master, width=cell_box[2])
-            # values recorded for _on_return_pressed
-            edit_entry.editing_column_index = col_number
-            edit_entry.editing_item_iid = selected_row_iid
-            # only cells are editable / not the tree part
-            if col_number > -1:
-                edit_entry.place(x=cell_box[0], y=cell_box[1], w=cell_box[2], h=cell_box[3])
-                print("winfo_rooty", self.winfo_rooty())
-                edit_entry.insert(0, cell_value)
-                edit_entry.select_range(0, END)
-                edit_entry.focus()
-                edit_entry.bind("<FocusOut>", self._on_focus_out)
-                edit_entry.bind("<Return>", self._on_return_pressed)
-                edit_entry.bind("<Escape>", self._on_focus_out)
+                print("cell_value", cell_value)
+        elif region_clicked == "tree":
+            col_number = -1
+            cell_value = selected_values.get("text")
+            if self.debug:
+                print("tree_value", cell_value)
+        else:
+            print("Header clicked")
+        #
+        self.edit_frame = Frame(self.master)
+        self.edit_entry = Entry(self.edit_frame, width=cell_box[2])
+        self.edit_entry.pack()
+        self.horscrlbar = Scrollbar(self.edit_frame, orient="horizontal", width=20, command=self.edit_entry.xview)
+        self.horscrlbar.pack(fill=BOTH, expand=1)
+        self.edit_entry.configure(xscrollcommand=self.horscrlbar.set)
+        # values recorded for _on_return_pressed
+        self.edit_entry.editing_column_index = col_number
+        self.edit_entry.editing_item_iid = selected_row_iid
+        # only cells are editable / not the tree part
+        if col_number > -1:
+            self.edit_entry.insert(0, cell_value)
+        else:
+            self.edit_entry.insert(0, cell_value)
+        self.edit_frame.place(x=cell_box[0], y=cell_box[1], w=cell_box[2], h=cell_box[3] * 2)
+        self.edit_entry.select_range(0, END)
+        self.edit_entry.focus()
+        self.edit_entry.bind("<FocusOut>", self._on_focus_out)
+        self.edit_entry.bind("<Return>", self._on_return_pressed)
+        self.edit_entry.bind("<Escape>", self._on_focus_out)
 
     def _on_focus_out(self, event):
         """
@@ -114,7 +192,10 @@ class mtkEditTable(Treeview):
         :param event:
         :return:
         """
-        event.widget.destroy()
+        self.edit_frame.destroy()
+        # self.horscrlbar = None
+        # self.edit_entry = None
+        # event.widget.destroy()
 
     def _on_return_pressed(self, event):
         """
@@ -132,7 +213,7 @@ class mtkEditTable(Treeview):
             self.item(selected_row_iid, values=values)
         else:
             self.item(selected_row_iid, text=new_text)
-        event.widget.destroy()
+        self.edit_frame.destroy()
         self.cells = self.get_data()
 
 
